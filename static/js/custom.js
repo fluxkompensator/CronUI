d6b3171e4813f0907e1673576521489ca0e1f378
fluxkompensator <flowhosts@gmail.com>
2017-09-19 00:06:53 +0200

    repo cleanup, added some comments

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
+    // add new Entries via button
     $( ".add-entry-btn" ).click(function() {
 	$( '<a href="#" class="list-group-item list-group-item-danger cron-entry">#* * * * * root ... Empty line</a>' ).insertBefore( this );
 	$( '<input type="text" class="cron-edit" style="display:none"/>' ).insertBefore( this );
 	$( '<button class="cron-edit cron-delete btn btn-sm btn-warning" style="display:none">Delete entry</button>' ).insertBefore( this );
-        console.log($( this ));
     });
 
+    // show options/actions for clicked entry
     $(document).on( "click", '.cron-entry', function(){
         $(this).hide().next(".cron-edit").show().val($.trim($(this).text())).focus();
 	$(this).next().next(".cron-edit").show();
@@ -13,28 +14,28 @@ $(document).ready(function(){
 
     // "mousedown" is much better than "onclick/click" it does not interfere with "focusout"
     $(document).on( "mousedown", ".cron-delete", function(){
-	console.log($(this));
 	$(this).prev().prev(".cron-entry").remove();
 	$(this).prev().remove();
 	$(this).remove();
     });
 
+    // uncoollapse current item on focusout
     $(".list-group").on('focusout', '.cron-edit', function(){
-        console.log($(this));
         $(this).hide().prev(".cron-entry").show().text($(this).val());
 	$(this).next('.cron-delete').hide();
 	$(this).next('.cron-edit').hide();
     });
 
+    // terminate the edit field by pressing enter
     $(document).on('keypress', '.cron-edit', function( event ){
 	if(event.which == 13) {
             $(this).hide().prev(".cron-entry").show().text($(this).val());
     	    $(this).next('.cron-delete').hide();
     	    $(this).next('.cron-edit').hide();
-            console.log(event.which);
 	}
     });
 
+    // save cron entry POST request via AJAX  
     $(function() {
         $('.save-entry-btn').click(function() {
 	    var cronEntries = [];
@@ -49,17 +50,16 @@ $(document).ready(function(){
                 contentType: 'application/json;charset=UTF-8',
                 type: 'POST',
                 success: function(response) {
-                    console.log(response);
 		    alert("Crontab "+cronTitle+" saved.");
                 },
                 error: function(error) {
-                    console.log(error);
 		    alert("Crontab "+cronTitle+" save failed!");
                 }
             });
         });
     });
 
+    // sets active class in navigation item
     $(function() {
         $('nav a[href^="' + location.pathname.split("/")[1] + '"]').addClass('active');
     });
